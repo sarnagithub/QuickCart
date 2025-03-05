7f2c4bceaf5458c22e7c5364b458923c7d0cc485
SARNADIP CHANDRA <146744487+sarnagithub@users.noreply.github.com>
2025-03-05 10:32:06 +0530

    Add files via upload

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+      },
+    },
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+  },
+];
+
+export default eslintConfig;
